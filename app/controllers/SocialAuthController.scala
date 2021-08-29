1bb91f6cb4d2634de83c3167b5b697a695cad600
Name <36419747+pineapple333@users.noreply.github.com>
2021-08-29 23:44:12 +0200

    Changed redirect after a successful social authentication

--- app/controllers/SocialAuthController.scala
+++ app/controllers/SocialAuthController.scala
@@ -22,7 +22,7 @@ class SocialAuthController @Inject()(scc: DefaultSilhouetteControllerComponents,
             _ <- authInfoRepository.save(profile.loginInfo, authInfo)
             authenticator <- authenticatorService.create(profile.loginInfo)
             value <- authenticatorService.init(authenticator)
-            result <- authenticatorService.embed(value, Redirect("http://localhost:12346"))
+            result <- authenticatorService.embed(value, Redirect("https://cherrybookstore-front-ul5xycu2sa-uc.a.run.app"))
           } yield {
             val Token(name, value) = CSRF.getToken.get
             result.withCookies(Cookie(name, value, httpOnly = false), 
